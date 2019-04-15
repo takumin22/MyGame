c24a0f3285f6476d93d062299741aab9bb46d9f7
takumin22 <takumibakuhtu@gmail.com>
2019-04-15 11:09:31 +0900

    フェード実装

--- GameTemplate/Game/Fade.cpp
+++ GameTemplate/Game/Fade.cpp
@@ -1,22 +1,48 @@
 #include "stdafx.h"
 #include "Fade.h"
-
+#include "Sprite.h"
 
 Fade::Fade()
 {
+	//フェード用のデータを読み込み
+	m_texture_fade.Init(L"Resource/sprite/White.dds", 1280.0f, 720.0f);
+	m_texture_fade.SetclearColor(0.0f);
 }
 
 
 Fade::~Fade()
 {
 }
-
-void Fade::Update()
+bool Fade::Update()
 {
-
+	m_texture_fade.SetclearColor(m_toumei);
+	//状態の遷移
+	switch (m_fadestate)
+	{
+	case fadein:
+		if (m_toumei >= 1.0f)
+		{
+			m_toumei = 1.0f;
+			return true;
+		}
+		break;
+	case fadeout:
+		if (m_toumei <= 0.0f)
+		{
+			m_toumei = 0.0f;
+			return true;
+		}
+		break;
+	case idel:
+		return false;
+		break;
+	default:
+		break;
+	}
+	m_toumei += m_faderate;
+	return false;
 }
-
 void Fade::Draw()
 {
-
+	m_texture_fade.Draw();
 }
--- GameTemplate/Game/Fade.h
+++ GameTemplate/Game/Fade.h
@@ -1,12 +1,82 @@
 #pragma once
+/// <summary>
+/// フェイドクラス
+/// </summary>
 class Fade
 {
 public:
+	/// <summary>
+	/// コンストラクタ
+	/// </summary>
 	Fade();
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
 	~Fade();
-	void Update();
+	/// <summary>
+	/// フェイドの状態のEnum
+	/// </summary>
+	enum Fade_State
+	{
+		fadein,
+		fadeout,
+		idel
+	};
+	/// <summary>
+	/// Update関数
+	/// 状態変化でtrue
+	/// </summary>
+	bool Update();
+	/// <summary>
+	/// Draw関数
+	/// </summary>
 	void Draw();
+	/// <summary>
+	/// フェードインに遷移
+	/// </summary>
+	/// <param name="taim">
+	/// フェードにかける時間（1/taim）
+	/// </param>
+	void Fadein(float taim = 20.0f)
+	{
+		m_faderate = 1 / taim;
+		m_fadestate = fadein;
+	}
+	/// <summary>
+	/// フェードアウトに遷移
+	/// </summary>
+	/// <param name="taim">
+	/// フェードにかける時間（1/taim）
+	/// </param>
+	void Fadeout(float taim = 20.0f)
+	{
+		m_faderate = 1 / taim;
+		m_faderate *= -1;
+		m_fadestate = fadeout;
+	}
+	/// <summary>
+	/// フェードをやめてアイドルに遷移
+	/// </summary>
+	void Fadeidel()
+	{
+		m_faderate = 0.0f;
+		m_fadestate = idel;
+	}
+	/// <summary>
+	/// 現在の状態を取得
+	/// </summary>
+	/// <returns>
+	/// 状態のEnum（Faid::Fade_State）
+	/// </returns>
+	const Fade_State GetState()
+	{
+		return m_fadestate;
+	}
 private:
-
+	Sprite m_texture_fade;							//タイトルのフェイド用のリソース
+	//Sprite m_fadeSprite;										//タイトルのフェイド表示用のインスタンス
+	float m_faderate = 0.0f;									//フェイドをかける時間
+	float m_toumei = 0.0f;										//フェイドの度合い
+	Fade_State m_fadestate = idel;								//現在の状態
 };
 
